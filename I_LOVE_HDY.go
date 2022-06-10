461f03a3d6edef00fc9dbd86095c75271e267709
sorecrer <1037059224@qq.com>
2022-06-10 21:55:36 +0800

    import fmt

--- I_LOVE_HDY.go
+++ I_LOVE_HDY.go
@@ -1,6 +1,12 @@
 package main
 
+import "fmt"
+
 func main() {
-	print("I really Love Hdy!!!,very love!!")
-	pring("Super AI")
+	fmt.Println("I really Love Hdy!!!,very love!!")
+	fmt.Println("Super AI")
 }
+
+//[7,3,4,3,1,7,1] >> 4
+//[1,2,1,3,2,1,1] >> 3
+//[7,5,2,5,4,1,7] >> 6
